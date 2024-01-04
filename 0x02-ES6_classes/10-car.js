2d8afddca6c10ec2f5b318237fe69b3941a6080f
KollihTheGuruh <mmike8449@gmail.com>
2024-01-05 00:14:41 +0300

    Vroom

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,13 +1,29 @@
-/* eslint-disable */
-import Car from './10-car';
+import Car from "./10-car.js";
 
-export default class EVCar extends Car {
-	constructor(brand, motor, color, range) {
-		super(brand, motor, color);
-		this._range = range;
-	}
+class TestCar extends Car {}
 
-	cloneCar() {
-		return new Car();
-	}
+const tc1 = new TestCar("Nissan", "Turbo", "Pink");
+const tc2 = tc1.cloneCar();
+
+console.log(tc1);
+console.log(tc1 instanceof TestCar);
+
+console.log(tc2);
+console.log(tc2 instanceof TestCar);
+
+console.log(tc1 == tc2);
+
+// 10-car.js
+class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    return new this.constructor(this._brand, this._motor, this._color);
+  }
 }
+
+export default Car;
